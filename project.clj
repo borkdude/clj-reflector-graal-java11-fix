382c70e31ffa3aac240d366db8917bfeabc3645e
Michiel Borkent <michielborkent@gmail.com>
2020-03-09 19:54:00 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject borkdude/clj-reflector-graal-java11-fix "0.0.1-graalvm-19.3.1-alpha.1"
+  :license {:name "Eclipse Public License 1.0"
+            :url "http://opensource.org/licenses/eclipse-1.0.php"}
+  :url "https://github.com/borkdude/clj-reflector-graal-java11-fix"
+  :scm {:name "git"
+        :url "https://github.com/borkdude/clj-reflector-graal-java11-fix"}
+  :dependencies [[org.clojure/clojure "1.10.2-alpha1"]
+                 [org.graalvm.nativeimage/svm "19.3.1"]]
+  :java-source-paths ["src-java"]
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :username :env/clojars_user
+                                    :password :env/clojars_pass
+                                    :sign-releases false}]])
